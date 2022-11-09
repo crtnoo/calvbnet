cb90f68a4ba2b781b91f22d66aa072486f7c1604
crtnoo <91428713+crtnoo@users.noreply.github.com>
2022-11-09 09:50:27 -0300

    Add files via upload

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,271 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.bt7 = New System.Windows.Forms.Button()
+        Me.bt8 = New System.Windows.Forms.Button()
+        Me.bt9 = New System.Windows.Forms.Button()
+        Me.btdivision = New System.Windows.Forms.Button()
+        Me.btresta = New System.Windows.Forms.Button()
+        Me.bt6 = New System.Windows.Forms.Button()
+        Me.bt5 = New System.Windows.Forms.Button()
+        Me.bt4 = New System.Windows.Forms.Button()
+        Me.btmultiplicacion = New System.Windows.Forms.Button()
+        Me.bt3 = New System.Windows.Forms.Button()
+        Me.bt2 = New System.Windows.Forms.Button()
+        Me.bt1 = New System.Windows.Forms.Button()
+        Me.btsuma = New System.Windows.Forms.Button()
+        Me.btpunto = New System.Windows.Forms.Button()
+        Me.bt0 = New System.Windows.Forms.Button()
+        Me.btigual = New System.Windows.Forms.Button()
+        Me.btlimpiar = New System.Windows.Forms.Button()
+        Me.tbresultado = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'bt7
+        '
+        Me.bt7.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt7.Location = New System.Drawing.Point(34, 95)
+        Me.bt7.Name = "bt7"
+        Me.bt7.Size = New System.Drawing.Size(70, 65)
+        Me.bt7.TabIndex = 0
+        Me.bt7.Text = "7"
+        Me.bt7.UseVisualStyleBackColor = True
+        '
+        'bt8
+        '
+        Me.bt8.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt8.Location = New System.Drawing.Point(110, 95)
+        Me.bt8.Name = "bt8"
+        Me.bt8.Size = New System.Drawing.Size(70, 65)
+        Me.bt8.TabIndex = 1
+        Me.bt8.Text = "8"
+        Me.bt8.UseVisualStyleBackColor = True
+        '
+        'bt9
+        '
+        Me.bt9.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt9.Location = New System.Drawing.Point(186, 95)
+        Me.bt9.Name = "bt9"
+        Me.bt9.Size = New System.Drawing.Size(70, 65)
+        Me.bt9.TabIndex = 2
+        Me.bt9.Text = "9"
+        Me.bt9.UseVisualStyleBackColor = True
+        '
+        'btdivision
+        '
+        Me.btdivision.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btdivision.Location = New System.Drawing.Point(262, 95)
+        Me.btdivision.Name = "btdivision"
+        Me.btdivision.Size = New System.Drawing.Size(70, 65)
+        Me.btdivision.TabIndex = 3
+        Me.btdivision.Text = "/"
+        Me.btdivision.UseVisualStyleBackColor = True
+        '
+        'btresta
+        '
+        Me.btresta.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btresta.Location = New System.Drawing.Point(262, 166)
+        Me.btresta.Name = "btresta"
+        Me.btresta.Size = New System.Drawing.Size(70, 65)
+        Me.btresta.TabIndex = 7
+        Me.btresta.Text = "-"
+        Me.btresta.UseVisualStyleBackColor = True
+        '
+        'bt6
+        '
+        Me.bt6.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt6.Location = New System.Drawing.Point(186, 166)
+        Me.bt6.Name = "bt6"
+        Me.bt6.Size = New System.Drawing.Size(70, 65)
+        Me.bt6.TabIndex = 6
+        Me.bt6.Text = "6"
+        Me.bt6.UseVisualStyleBackColor = True
+        '
+        'bt5
+        '
+        Me.bt5.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt5.Location = New System.Drawing.Point(110, 166)
+        Me.bt5.Name = "bt5"
+        Me.bt5.Size = New System.Drawing.Size(70, 65)
+        Me.bt5.TabIndex = 5
+        Me.bt5.Text = "5"
+        Me.bt5.UseVisualStyleBackColor = True
+        '
+        'bt4
+        '
+        Me.bt4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt4.Location = New System.Drawing.Point(34, 166)
+        Me.bt4.Name = "bt4"
+        Me.bt4.Size = New System.Drawing.Size(70, 65)
+        Me.bt4.TabIndex = 4
+        Me.bt4.Text = "4"
+        Me.bt4.UseVisualStyleBackColor = True
+        '
+        'btmultiplicacion
+        '
+        Me.btmultiplicacion.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btmultiplicacion.Location = New System.Drawing.Point(262, 237)
+        Me.btmultiplicacion.Name = "btmultiplicacion"
+        Me.btmultiplicacion.Size = New System.Drawing.Size(70, 65)
+        Me.btmultiplicacion.TabIndex = 11
+        Me.btmultiplicacion.Text = "x"
+        Me.btmultiplicacion.UseVisualStyleBackColor = True
+        '
+        'bt3
+        '
+        Me.bt3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt3.Location = New System.Drawing.Point(186, 237)
+        Me.bt3.Name = "bt3"
+        Me.bt3.Size = New System.Drawing.Size(70, 65)
+        Me.bt3.TabIndex = 10
+        Me.bt3.Text = "3"
+        Me.bt3.UseVisualStyleBackColor = True
+        '
+        'bt2
+        '
+        Me.bt2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt2.Location = New System.Drawing.Point(110, 237)
+        Me.bt2.Name = "bt2"
+        Me.bt2.Size = New System.Drawing.Size(70, 65)
+        Me.bt2.TabIndex = 9
+        Me.bt2.Text = "2"
+        Me.bt2.UseVisualStyleBackColor = True
+        '
+        'bt1
+        '
+        Me.bt1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt1.Location = New System.Drawing.Point(34, 237)
+        Me.bt1.Name = "bt1"
+        Me.bt1.Size = New System.Drawing.Size(70, 65)
+        Me.bt1.TabIndex = 8
+        Me.bt1.Text = "1"
+        Me.bt1.UseVisualStyleBackColor = True
+        '
+        'btsuma
+        '
+        Me.btsuma.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btsuma.Location = New System.Drawing.Point(262, 308)
+        Me.btsuma.Name = "btsuma"
+        Me.btsuma.Size = New System.Drawing.Size(70, 65)
+        Me.btsuma.TabIndex = 15
+        Me.btsuma.Text = "+"
+        Me.btsuma.UseVisualStyleBackColor = True
+        '
+        'btpunto
+        '
+        Me.btpunto.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btpunto.Location = New System.Drawing.Point(186, 308)
+        Me.btpunto.Name = "btpunto"
+        Me.btpunto.Size = New System.Drawing.Size(70, 65)
+        Me.btpunto.TabIndex = 14
+        Me.btpunto.Text = "."
+        Me.btpunto.UseVisualStyleBackColor = True
+        '
+        'bt0
+        '
+        Me.bt0.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bt0.Location = New System.Drawing.Point(34, 308)
+        Me.bt0.Name = "bt0"
+        Me.bt0.Size = New System.Drawing.Size(146, 65)
+        Me.bt0.TabIndex = 12
+        Me.bt0.Text = "0"
+        Me.bt0.UseVisualStyleBackColor = True
+        '
+        'btigual
+        '
+        Me.btigual.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btigual.Location = New System.Drawing.Point(338, 166)
+        Me.btigual.Name = "btigual"
+        Me.btigual.Size = New System.Drawing.Size(70, 207)
+        Me.btigual.TabIndex = 17
+        Me.btigual.Text = "="
+        Me.btigual.UseVisualStyleBackColor = True
+        '
+        'btlimpiar
+        '
+        Me.btlimpiar.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btlimpiar.Location = New System.Drawing.Point(338, 95)
+        Me.btlimpiar.Name = "btlimpiar"
+        Me.btlimpiar.Size = New System.Drawing.Size(70, 65)
+        Me.btlimpiar.TabIndex = 16
+        Me.btlimpiar.Text = "C"
+        Me.btlimpiar.UseVisualStyleBackColor = True
+        '
+        'tbresultado
+        '
+        Me.tbresultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 38.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbresultado.Location = New System.Drawing.Point(34, 24)
+        Me.tbresultado.Name = "tbresultado"
+        Me.tbresultado.Size = New System.Drawing.Size(374, 65)
+        Me.tbresultado.TabIndex = 19
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(449, 424)
+        Me.Controls.Add(Me.tbresultado)
+        Me.Controls.Add(Me.btigual)
+        Me.Controls.Add(Me.btlimpiar)
+        Me.Controls.Add(Me.btsuma)
+        Me.Controls.Add(Me.btpunto)
+        Me.Controls.Add(Me.bt0)
+        Me.Controls.Add(Me.btmultiplicacion)
+        Me.Controls.Add(Me.bt3)
+        Me.Controls.Add(Me.bt2)
+        Me.Controls.Add(Me.bt1)
+        Me.Controls.Add(Me.btresta)
+        Me.Controls.Add(Me.bt6)
+        Me.Controls.Add(Me.bt5)
+        Me.Controls.Add(Me.bt4)
+        Me.Controls.Add(Me.btdivision)
+        Me.Controls.Add(Me.bt9)
+        Me.Controls.Add(Me.bt8)
+        Me.Controls.Add(Me.bt7)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents bt7 As System.Windows.Forms.Button
+    Friend WithEvents bt8 As System.Windows.Forms.Button
+    Friend WithEvents bt9 As System.Windows.Forms.Button
+    Friend WithEvents btdivision As System.Windows.Forms.Button
+    Friend WithEvents btresta As System.Windows.Forms.Button
+    Friend WithEvents bt6 As System.Windows.Forms.Button
+    Friend WithEvents bt5 As System.Windows.Forms.Button
+    Friend WithEvents bt4 As System.Windows.Forms.Button
+    Friend WithEvents btmultiplicacion As System.Windows.Forms.Button
+    Friend WithEvents bt3 As System.Windows.Forms.Button
+    Friend WithEvents bt2 As System.Windows.Forms.Button
+    Friend WithEvents bt1 As System.Windows.Forms.Button
+    Friend WithEvents btsuma As System.Windows.Forms.Button
+    Friend WithEvents btpunto As System.Windows.Forms.Button
+    Friend WithEvents bt0 As System.Windows.Forms.Button
+    Friend WithEvents btigual As System.Windows.Forms.Button
+    Friend WithEvents btlimpiar As System.Windows.Forms.Button
+    Friend WithEvents tbresultado As System.Windows.Forms.TextBox
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,124 @@
+﻿Public Class Form1
+    Dim Operacion As String
+    Dim ValorResultado As Nullable(Of Double) = Nothing
+    Dim Valor2 As Nullable(Of Double) = Nothing
+    Dim Bandera As Boolean
+    Public Sub OperacionProceso()
+        Bandera = True
+        Valor2 = Val(tbresultado.Text)
+        If ValorResultado IsNot Nothing Then
+            Select Case Operacion
+                Case "+"
+                    ValorResultado = ValorResultado + Valor2
+                Case "-"
+                    ValorResultado -= Valor2
+                Case "*"
+                    ValorResultado *= Valor2
+                Case "/"
+                    ValorResultado /= Valor2
+            End Select
+            tbresultado.Text = ValorResultado
+        Else
+            ValorResultado = Valor2
+        End If
+    End Sub
+    Public Sub DeterminarConcatenar()
+        If Bandera = True Then
+            tbresultado.Text = ""
+            Bandera = False
+        ElseIf tbresultado.Text = "0" Then
+            tbresultado.Text = ""
+        End If
+    End Sub
+
+    Private Sub bt7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt7.Click
+        DeterminarConcatenar()
+        tbresultado.Text &= "7"
+    End Sub
+
+    Private Sub bt9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt9.Click
+        DeterminarConcatenar()
+        tbresultado.Text &= "9"
+    End Sub
+
+    Private Sub bt6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt6.Click
+        DeterminarConcatenar()
+        tbresultado.Text &= "6"
+    End Sub
+
+    Private Sub btsuma_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btsuma.Click
+        OperacionProceso()
+        Operacion = "+"
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbresultado.TextChanged
+
+    End Sub
+
+    Private Sub bt0_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles bt0.Click
+        DeterminarConcatenar()
+        tbresultado.Text &= "0"
+    End Sub
+
+    Private Sub bt1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt1.Click
+        DeterminarConcatenar()
+        tbresultado.Text &= "1"
+    End Sub
+
+    Private Sub bt2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt2.Click
+        DeterminarConcatenar()
+        tbresultado.Text &= "2"
+    End Sub
+
+    Private Sub bt3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt3.Click
+        DeterminarConcatenar()
+        tbresultado.Text &= "3"
+    End Sub
+
+    Private Sub bt4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt4.Click
+        DeterminarConcatenar()
+        tbresultado.Text &= "4"
+    End Sub
+
+    Private Sub bt5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt5.Click
+        DeterminarConcatenar()
+        tbresultado.Text &= "5"
+    End Sub
+
+    Private Sub bt8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bt8.Click
+        DeterminarConcatenar()
+        tbresultado.Text &= "8"
+    End Sub
+
+    Private Sub btpunto_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btpunto.Click
+        DeterminarConcatenar()
+        If InStr(tbresultado.Text, ".", CompareMethod.Text) = 0 Then tbresultado.Text &= "."
+
+    End Sub
+
+    Private Sub btmultiplicacion_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btmultiplicacion.Click
+        OperacionProceso()
+        Operacion = "*"
+    End Sub
+
+    Private Sub btresta_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btresta.Click
+        OperacionProceso()
+        Operacion = "-"
+    End Sub
+
+    Private Sub btdivision_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btdivision.Click
+        OperacionProceso()
+        Operacion = "/"
+    End Sub
+
+    Private Sub btigual_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btigual.Click
+        OperacionProceso()
+        Operacion = "="
+    End Sub
+
+    Private Sub btlimpiar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btlimpiar.Click
+        tbresultado.Text = "0"
+        Valor2 = Nothing
+        ValorResultado = Nothing
+    End Sub
+End Class
